aa6a2ca5b9ef109cd1b38713f39a1cc19c320b7f
artisanmuhammadali <artisanmuhammadali@gmail.com>
2025-01-30 20:53:48 +0500

    init

---
+++ app/Models/NumberVerification.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NumberVerification extends Model
+{
+    protected $fillable =[
+        'number',
+        'code',
+        'callSid',
+        'status',
+        'response',
+        'user_id',
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Models\NumberVerification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+use Twilio\Rest\Client;
+use Twilio\TwiML\VoiceResponse;
+
+Route::get('/', function () {
+    return 'welcome';
+});
+Route::get('/verify-number/{phone_number}', function () {
+    $number = request()->phone_number;
+    
+    $sid = env('TWILIO_ACC_ID');
+    $token = env('TWILIO_AUTH_TOKEN');
+    $twilio = new Client($sid, $token);
+    $validation_request = $twilio->validationRequests->create(
+        "+".$number,
+        [
+            "friendlyName" => "Third Party VOIP Number",
+            "statusCallback" => "https://3dcd-182-191-73-121.ngrok-free.app/receive-verification-callback",
+        ]
+    );
+    $data = $validation_request->toArray();
+    NumberVerification::create([
+        'number'=> $number,
+        'code'=>$data['validationCode'],
+        'callSid'=>$data['callSid']
+    ]);
+    return $data;
+});
+
+
+Route::any('/listen-to-twilio-verification-call' , function(Request $request){
+    // 14157234000
+    Log::info('listen-to-twilio-verification-call');
+    Log::info($request);
+    Log::info($request->ip());
+    // $response = new VoiceResponse();
+    // $response->say('12', ['loop' => 2]);
+    // $response->play('12', ['digits' => '12']);
+    // return $response;
+});
+
+Route::any('/listen-to-twilio-verification-call-failed' , function(Request $request){
+    // 14157234000
+    Log::info('listen-to-twilio-verification-call-failed');
+    Log::info($request->ip());
+});
+Route::any('/listen-to-twilio-verification-call-progress' , function(Request $request){
+    // 14157234000
+    Log::info('listen-to-twilio-verification-call-progress');
+    Log::info($request->ip());
+});
+
+
+Route::any('/receive-verification-callback' , function(Request $request){
+    Log::info('receive-verification-callback');
+    NumberVerification::where('CallSid', $request->CallSid)->update(['response'=>json_encode($request->all()) , 'status'=>$request->VerificationStatus]);
+    Log::info($request);
+});
