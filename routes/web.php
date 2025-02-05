4a8a8a4b186c9d6a0f4a9df4671a8a8189728cc9
artisanmuhammadali <artisanmuhammadali@gmail.com>
2025-02-05 17:25:44 +0500

    attend call testing

--- routes/web.php
+++ routes/web.php
@@ -106,8 +106,8 @@ Route::any('/listen-to-twilio-verification-call', function(Request $request) {
                     'Accept' => 'application/json',
                     'Authorization' => "Bearer $token",
                 ])->post("https://api.telnyx.com/v2/calls/{$callControlId}/actions/speak", [
-                    "payload" => "Your verification code is " . $verification->code_as_text,
-                    "voice" => "Polly.Joanna" // Use a valid Telnyx voice
+                    "payload" => $verification->code_as_text,
+                    "voice" => "Polly.Joanna"
                 ]);
                 Log::info("Speaking OTP: " . $response->body());
 
